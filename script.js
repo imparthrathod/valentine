8fca03addf4e51fada092df72e761e28a503d5f3
Parth Rathod <130079135+imparthrathod@users.noreply.github.com>
2025-02-09 13:58:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const nextPageUrl = 'main.html'; // Replace with the URL of the new page
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
 	const userInput = codeInput.value.trim();
-	if (userInput === 'Zalak' || userInput === 'zalak' || userInput === 'Rina' || userInput === 'rina' || userInput === 'Rini'userInput === 'rini') {
+	if (userInput === 'Zalak' || userInput === 'zalak' || userInput === 'Rina' || userInput === 'rina' || userInput === 'Rini' || userInput === 'rini') {
 		resultDiv.innerHTML = 'Yes you are my Baby';
 		window.location.href = nextPageUrl; // Redirect to new page
 	} else {
